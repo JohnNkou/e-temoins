e93598dd02287f2d13f321733310ed676982a3a1
alinekashoba <67825625+alinekashoba@users.noreply.github.com>
2023-11-22 16:46:50 +0100

    updated the host name

--- src/names.js
+++ src/names.js
@@ -1,3 +1,3 @@
-export const host = 't.com'
-export const upload = `upload/${host}`;
+export const host = 'e-path.space'
+export const upload = `upload.${host}`;
 export const success = `${host}/success.php`
