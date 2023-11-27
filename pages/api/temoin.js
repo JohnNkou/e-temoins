72d6a28e14333e0f5369b5976d3972317fe6e2a4
alinekashoba <67825625+alinekashoba@users.noreply.github.com>
2023-11-27 12:42:16 +0100

    added hashing of password

--- pages/api/temoin.js
+++ pages/api/temoin.js
@@ -2,8 +2,21 @@ import conn from '../../src/db/db.js';
 import dbClass from '../../src/db/dbClass.js'
 import { multiParse } from '../../src/helper.js'
 import fs from 'fs/promises'
+import bcrypt from 'bcrypt';
 
 const ROOT = process.env.ROOT;
+
+function getHash(text){
+	return new Promise((resolve,reject)=>{
+		bcrypt.hash(text,10,function(err,hash){
+			if(err){
+				return reject(err);
+			}
+
+			resolve(hash);
+		})
+	})
+}
 
 export default async function Candidat(req,res){
 	let db = new dbClass(conn),
@@ -12,36 +25,44 @@ export default async function Candidat(req,res){
 	method = req.method,
 	form = await multiParse(req).catch((error)=> ({error}));
 
-	if(method == 'GET'){
-		await db.getTemoins(query).then((response)=>{
-			res.status(200).json(response);
-		}).catch((error)=>{
-			res.status(500).json(error);
-		})
-	}
-	else if(req.method == 'POST'){
-		console.log(form);
-		form.data.role = 'temoin';
-		if(!(form.data.idCandidats instanceof Array)){
-			form.data.idCandidats = [form.data.idCandidats];
+	try{
+		if(method == 'GET'){
+			await db.getTemoins(query).then((response)=>{
+				res.status(200).json(response);
+			}).catch((error)=>{
+				res.status(500).json(error);
+			})
 		}
-		await db.addUser(form.data).then(async (response)=>{
-			for(let i = 0; i < form.removePath.length; i++){
-				let path = form.removePath[i];
-				console.log('path',path);
-				console.log(await fs.rename(path[0],`${ROOT}/public/upload/${path[1]}`))
+		else if(req.method == 'POST'){
+			
+			form.data.role = 'temoin';
+			form.data.password = await getHash(form.data.password);
+
+			if(!(form.data.idCandidats instanceof Array)){
+				form.data.idCandidats = [form.data.idCandidats];
 			}
-			res.status((response.inserted)? 201:200).json(response);
-		}).catch(async (error)=>{
-			res.status(500).json(error);
-			for(let i=0; i < form.removePath.length; i++){
-				let path = form.removePath[i];
-				await fs.rm(path[0]);
-			}
-		})
+			await db.addUser(form.data).then(async (response)=>{
+				for(let i = 0; i < form.removePath.length; i++){
+					let path = form.removePath[i];
+					console.log('path',path);
+					console.log(await fs.rename(path[0],`${ROOT}/public/upload/${path[1]}`))
+				}
+				res.status((response.inserted)? 201:200).json(response);
+			}).catch(async (error)=>{
+				res.status(500).json(error);
+				for(let i=0; i < form.removePath.length; i++){
+					let path = form.removePath[i];
+					await fs.rm(path[0]);
+				}
+			})
+		}
+		else{
+			res.status(401);
+		}
 	}
-	else{
-		res.status(401);
+	catch(e){
+		console.error("Error",e);
+		res.status(500).json(e);
 	}
 }
 
